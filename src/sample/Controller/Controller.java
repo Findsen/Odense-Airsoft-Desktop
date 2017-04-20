808c755de27549e8d2631d1edd1cc6fd4e7b826e
Findsen <chr.findsen@gmail.com>
2017-04-20 08:12:40 +0200

    Til git

---
+++ src/sample/Controller/Controller.java
@@ -0,0 +1,69 @@
+package sample.Controller;
+
+
+
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+public class Controller{
+
+
+
+    public static Stage mainStage;
+    // Member
+    @FXML private TextField FirstName;
+    @FXML private TextField LastName;
+    @FXML private DatePicker datepicker_Birthday;
+
+    //Login
+    @FXML private PasswordField Login_passwordField;
+    @FXML private TextField Login_textField;
+
+    // Mainmenu
+
+
+
+
+    public static void initializeController(Stage stage){
+        mainStage = stage;
+    }
+
+   public void send (ActionEvent event)
+    {
+        System.out.println(FirstName.getText()+" " + LastName.getText()+" " + datepicker_Birthday.getValue());
+    }
+
+    public void clear(ActionEvent event) throws IOException
+    {
+        FirstName.clear();
+        LastName.clear();
+
+
+        videre();
+    }
+
+    public void videre() throws IOException
+    {
+        Parent mainMenu = FXMLLoader.load(getClass().getResource("View/menuMen.fxml"));
+        Scene mainScene = new Scene(mainMenu);
+        mainStage.setScene(mainScene);
+    }
+
+    public void loginPressed(ActionEvent event) throws IOException
+    {
+        Parent mainMenu = FXMLLoader.load(getClass().getResource("View/MainMenu.fxml"));
+        Scene mainScene = new Scene(mainMenu);
+        mainStage.setScene(mainScene);
+    }
+}
---
+++ src/sample/Main.java
@@ -0,0 +1,27 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import sample.Controller.Controller;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        Parent root = FXMLLoader.load(getClass().getResource("View/MainMenu.fxml"));
+
+        Controller.initializeController(primaryStage);
+        primaryStage.setTitle("Login Window - Odense Airsoft");
+        primaryStage.setScene(new Scene(root, 600,500 ));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
